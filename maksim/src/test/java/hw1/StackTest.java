65daa2cfdcdeb3477a2537fac421fbc3f0e640cb
Maksim-ya <Maksim-ya@ukr.net>
2016-12-08 17:34:14 +0200

    Change 17-33

--- maksim/src/test/java/hw1/StackTest.java
+++ maksim/src/test/java/hw1/StackTest.java
@@ -1,16 +1,40 @@
 package hw1;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+
+import org.junit.Test;
+
 
 public class StackTest {
 
-    @org.junit.Test
+
+
+
+
+
+    @Test
     public void testPush() throws Exception {
+        Stack stack = new Stack(10);
+        stack.push(1);
 
+        stack.push(2);
+
+        stack.push(3);
+        Assert.assertEquals(3,stack.pop());
+        Assert.assertEquals(2,stack.pop());
+        Assert.assertEquals(1,stack.pop());
     }
 
-    @org.junit.Test
+    @Test
     public void testPop() throws Exception {
+        Stack stack = new Stack(10);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.pop();
+        Assert.assertEquals(2,stack.pop());
 
     }
+
+
 }
