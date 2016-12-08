84b2a1d7d315a1a27d90dec4468c60d26150ac17
Maksim-ya <Maksim-ya@ukr.net>
2016-12-08 17:47:34 +0200

    Change 1747

--- maksim/src/main/java/hw2/Text.java
+++ maksim/src/main/java/hw2/Text.java
@@ -15,22 +15,27 @@ public class Text {
         this.text = text;
     }
 
-    public Map<String, Integer> maptosort() {
-        Map<String, Integer> map = new HashMap<>();
-        String[] array = text.split(" ");
-        for (int i = 0; i < array.length; i++) {
-            if (!map.containsKey(array[i])) {
-                map.put(array[i], 1);
-            } else {
-                map.put(array[i], map.get(array[i]) + 1);
+
+
+
+    public Map <String,Integer> countWords(){
+        Map< String,Integer> map = new HashMap<>();
+        String [] array = text.split(" ");
+        for (int i =0; i<array.length;i++){
+            if (!map.containsKey(array[i]))
+                map.put(array[i],1);
+            else {
+                map.put(array[i],map.get(array[i])+1);
+
+
             }
         }
         return map;
     }
 
     public static void main(String[] args) {
-        String text = "Hello lalala Hello";
+        String text = "Hello lalala Hello! Hello Hello";
 
-        System.out.println(new Text(text).maptosort());
+        System.out.println(new Text(text).countWords());
     }
 }
