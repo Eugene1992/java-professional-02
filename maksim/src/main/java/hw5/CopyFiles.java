a90a5a4840e7c6008cc286980b3ccda35ee4af24
Maksim-ya <Maksim-ya@ukr.net>
2016-12-14 14:40:00 +0200

    14-40

--- maksim/src/main/java/hw5/CopyFiles.java
+++ maksim/src/main/java/hw5/CopyFiles.java
@@ -1,43 +1,18 @@
 package hw5;
 
 import java.io.*;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Created by User on 12/12/2016.
  */
 public class CopyFiles {
     public static void main(String[] args) {
-        File  folder = new File("C:\\Users\\User\\IdeaProjects\\java-professional-02\\maksim\\src\\main\\java\\hw5");
-        File[] folderEntries = folder.listFiles();
+        File folder = new File("C:\\Intel");
 
-            for (File entry : folderEntries)
-            {
-                if (entry.getName().lastIndexOf(".txt")!=-1 )
-                {
-                    try (FileReader is = new FileReader(entry);
-                         RandomAccessFile os = new RandomAccessFile("C:\\Users\\User\\IdeaProjects\\java-professional-02\\maksim\\src\\main\\java\\hw5\\Files.txt","rw"))
-                    {
-                        BufferedReader reader = new BufferedReader(is);
-                        String s= reader.readLine();
-                        while (reader.readLine()!=null) {
-                            os.write(s.getBytes());
-                        }
-                    }
-                    catch (IOException e) {
-                        e.printStackTrace();
-                        }
-
-                }
-
-            }
-        }
-
-
-
-
-
-
-
+    }
 
 
 }
---
+++ maksim/src/main/java/hw6/MyJson.java
@@ -0,0 +1,17 @@
+package hw6;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by User on 14/12/2016.
+ */
+
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target(value = ElementType.FIELD)
+public @interface MyJson {
+
+
+}
