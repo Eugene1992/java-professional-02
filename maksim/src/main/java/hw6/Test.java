df554065d7a466cd9788db67b60746f6bc06885d
Maksim-ya <Maksim-ya@ukr.net>
2016-12-16 18:47:13 +0200

    hw6

--- maksim/src/main/java/hw6/Test.java
+++ maksim/src/main/java/hw6/Test.java
@@ -3,28 +3,53 @@ package hw6;
 /**
  * Created by User on 14/12/2016.
  */
+
 import java.lang.reflect.Field;
 
+
 public class Test {
-    public static void main(String[] args) throws Exception {
-        Employee ivan = new Employee("Ivan", "Ivanov",30,10000);
-        Class<? extends Employee> aClass = ivan.getClass();
+    public static void main(String[] args) {
+        Employee ivan = new Employee("Ivan", "Ivanov", 30, 10000);
+
+
+
+            try {
+                  System.out.println(myMetod(ivan));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+    }
+
+    public static String myMetod(Object obj) throws Exception {
+
+        Class<?> aClass = obj.getClass();
+        StringBuilder stringBuilder = new StringBuilder();
         if (!aClass.isAnnotationPresent(MyJson.class)) {
             throw new Exception();
         }
-        System.out.println("{ ");
+        stringBuilder.append("{ \n");
 
-       Field[] declaredFields = aClass.getDeclaredFields();
+        Field[] declaredFields = aClass.getDeclaredFields();
         for (Field declaredField : declaredFields) {
-             declaredField.setAccessible(true);
-            StringBuilder stringBuilder = new StringBuilder();
-           if (declaredField.isAnnotationPresent(MyField.class)) {
-                stringBuilder.append("\t").append(String.format("\"%s\" : \"%s\" ,", declaredField.getName(), declaredField.get(ivan))).append("\n");
+            declaredField.setAccessible(true);
+            if (declaredField.isAnnotationPresent(MyField.class)) {
+                stringBuilder.append("\t").append(String.format("\"%s\" : ", declaredField.getName()));
+                if (declaredField.getType().isPrimitive()) {
+                    stringBuilder.append(String.format("%s,", declaredField.get(obj)));
+                } else stringBuilder.append(String.format("\"%s\",", declaredField.get(obj)));
+                stringBuilder.append("\n");
+
             }
-            System.out.print(stringBuilder);
+
         }
-        System.out.println("}");
+
+        stringBuilder.deleteCharAt(stringBuilder.lastIndexOf(","));
+        stringBuilder.append("}");
+        return stringBuilder.toString();
     }
+
 
 
 }
